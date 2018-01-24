731405bbd0baed5534fe974c1355e10a9b6b559c
Tiago Alves <alvesjtiago@gmail.com>
2018-01-24 09:53:16 +0000

    Filter by country.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @apis = Api.all
+    @countries = Country.all
+    if params[:country].present?
+      @country = Country.find_by_slug(params[:country])
+      if @country.present?
+        @apis = @country.apis
+      else
+        redirect_to root_url
+      end
+    else
+      @apis = Api.all
+    end
   end
 end
